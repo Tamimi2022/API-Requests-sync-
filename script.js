836fa0aac1999edf29fbef5b696e0ba0c106f5a0
Farid Attamimi <tamimi2021farid@gmail.com>
2022-04-16 14:36:38 +0200

    2022

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Hey hello, I am the first.") 
+setTimeout(function(){ console.log("Hey hello, I am the second."); }, 2000);
+console.log("Hey hello, I am the third.") 
+
+// Answer no.3 Asynchronous , Because the second using Timeout and w'll come after the third
+
